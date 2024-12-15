54561081e4df41186efd1af393ff649f2ce10689
Alex Tanton <alex.c.tanton@gmail.com>
2024-12-14 19:13:14 -0500

    Delete LunarSimulator directory

--- LunarSimulator/LAC.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/LAC/Binaries/Linux/LAC-Linux-Shipping"
-"$UE4_PROJECT_ROOT/LAC/Binaries/Linux/LAC-Linux-Shipping" LAC "$@" 
