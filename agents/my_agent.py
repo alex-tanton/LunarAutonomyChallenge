00d33279d6fa3131a88e580adf55135f0457f1b0
Alex Tanton <alex.c.tanton@gmail.com>
2025-01-01 21:47:34 -0500

    Improved comment formatting and skeleton typing

--- agents/my_agent.py
+++ agents/my_agent.py
@@ -27,119 +27,165 @@
 TODO: Add a description of the agent.
 """
 
-
 ### Import used libraries. ###
 import carla
+from carla import SensorPosition, VehicleVelocityControl
 import cv2 as cv
 import numpy as np
+
 # TODO Add any other used libraries.
 
 ### Import the AutonomousAgent from the Leaderboard. ###
-from leaderboard.autoagents.autonomous_agent.AutonomousAgent import AutonomousAgent
+from Leaderboard.leaderboard.autoagents.autonomous_agent import AutonomousAgent
 
 
-def get_entry_point():
+def get_entry_point() -> str:
     """
-    This function returns the name of the agent class. It will be used by the Leaderboard to instantiate an agent at
-    the beginning of mission execution.
+    This function returns the name of the agent class. It will be used by the
+    Leaderboard to instantiate an agent at the beginning of mission execution.
 
     Returns:
-    str: The name of the agent class.
+        str: The name of the agent class.
     """
-    return 'my_agent'
+
+    return "my_agent"
 
 
 class my_agent(AutonomousAgent):
 
-    def setup(self, path_to_conf_file):
+    def setup(self, path_to_conf_file: str) -> None:
         """
-        Defines the setup procedure to initialize the agent.
+        Setup function to initialize the agent.
 
-        All procedures necessary to initialize the control agent are executed here. This includes the initialization of
-        the agent's sensors and other variables, as well as the loading of any pre-trained models.
+        All procedures necessary to initialize the control agent are executed
+        here. This includes the initialization of the agent's sensors and other
+        variables, as well as the loading of any pre-trained models.
 
-        It is recommended to call the get_initial_position() method at some early point in the mission to begin the mapping 
-        process with the correct initial position of the robot.
+        It is recommended to call the get_initial_position() method at some early
+        point in the mission to begin the mapping process with the correct
+        initial position of the robot.
 
-        Parameters:
-        path_to_conf_file (str): Optionally used to refer to any configuration file for setting up your agent. For example,
-        it could be a JSON file containing the location of a network weights file or configuration parameters for controllers.
+        Args:
+            path_to_conf_file (str): Path to the configuration file. This is an
+            optional argument and can be ignored if not needed.
         """
 
         # TODO: Add any setup code needed for the agent.
 
         return
 
-    def use_fiducials(self):
+    def use_fiducials(self) -> bool:
         """
-        Defines whether the agent uses fiducial markers for navigation. Extra points are scored for not using fiducials.
-        
+        Defines whether the agent uses fiducial markers for navigation. Extra
+        points are scored for not using fiducials.
+
         Returns:
-        bool: True if fiducials are used, False otherwise.
+            bool: True if fiducials are used, False otherwise.
         """
 
         return True
 
-    def sensors(self):
+    def sensors(self) -> dict[SensorPosition, dict[str, any]]:
         """
-        Sets the initial sensor configuration of the agent. By default, all sensors are disabled.
+        Sets the initial sensor configuration of the agent. By default, all
+        sensors are disabled.
 
-        Each sensor position is initialized with a dictionary containing keys for the state of the camera, the light, and
-        also the width and height in pixels of the camera.
+        Each sensor position is initialized with a dictionary containing keys
+        for the state of the camera, the light, and also the width and height in
+        pixels of the camera.
 
-        You can activate and deactivate the cameras and lights later in the simulation using the relevant setter methods
-        of the API. 
+        You can activate and deactivate the cameras and lights later in the
+        simulation using the relevant setter methods of the API.
 
-        The use_semantic argument is optional, to activate the  semantic counterpart of each camera, it is False by default
-        if not set. The maximum camera resolution permitted is 2448 x 2048 pixels, if a higher resolution is requested the
-        resolution will be clipped to the maximum and a warning  will be given on the command line.
+        The use_semantic argument is optional, to activate the  semantic
+        counterpart of each camera, it is False by default if not set. The
+        maximum camera resolution permitted is 2448 x 2048 pixels, if a higher
+        resolution is requested the resolution will be clipped to the maximum and
+        a warning  will be given on the command line.
 
         Returns:
-        dict: A dictionary of sensors to be used, with keys corresponding to sensor positions and values containing sensor
-        configuration.
+            dict: A dictionary of sensors to be used, with keys corresponding to
+            sensor positions and values containing sensor configuration.
         """
 
-        return {
-            carla.SensorPosition.Front: {
-                'camera_active': True, 'light_intensity': 1.0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.FrontLeft: {
-                'camera_active': True, 'light_intensity': 1.0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.FrontRight: {
-                'camera_active': True, 'light_intensity': 1.0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.Left: {
-                'camera_active': False, 'light_intensity': 0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.Right: {
-                'camera_active': False, 'light_intensity': 0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.BackLeft: {
-                'camera_active': False, 'light_intensity': 0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.BackRight: {
-                'camera_active': False, 'light_intensity': 0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
-            carla.SensorPosition.Back: {
-                'camera_active': False, 'light_intensity': 0, 
-                'width': '2448', 'height': '2048', 'use_semantic': False},
+        sensors = {
+            SensorPosition.Front: {
+                "camera_active": True,
+                "light_intensity": 1.0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.FrontLeft: {
+                "camera_active": True,
+                "light_intensity": 1.0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.FrontRight: {
+                "camera_active": True,
+                "light_intensity": 1.0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.Left: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.Right: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.BackLeft: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.BackRight: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
+            SensorPosition.Back: {
+                "camera_active": False,
+                "light_intensity": 0,
+                "width": "2448",
+                "height": "2048",
+                "use_semantic": False,
+            },
         }
 
-    def run_step(self, input_data):
+        return sensors
+
+    def run_step(self, input_data) -> VehicleVelocityControl:
         """
         Implements the logic for a simulation time-step.
 
-        The simulator runs in discrete time steps at 20Hz in simulation time. This method is executed with each simulation
-        time-step by the Leaderboard. It processes sensor data and returns a control instruction to the simulator to move
-        or modify the robot's state. Cameras run at 10Hz, so camera data is delivered every other time-step.
+        The simulator runs in discrete time steps at 20Hz in simulation time.
+        This method is executed with each simulation time-step by the Leaderboard.
+        It processes sensor data and returns a control instruction to the
+        simulator to move or modify the robot's state. Cameras run at 10Hz, so
+        camera data is delivered every other time-step.
 
         Parameters:
-        input_data (dict): A dictionary containing sensor data from the previous simulation time-step. The dictionary has
-        two keys:
+        input_data (dict): A dictionary containing sensor data from the previous
+        simulation time-step. The dictionary has two keys:
             - 'Grayscale': Access the standard, grayscale camera data.
             - 'Semantic': Access the semantic segmentation data.
-        Each of these keys contains another dictionary with keys corresponding to the sensor positions:
+        Each of these keys contains another dictionary with keys corresponding
+        to the sensor positions:
             - carla.SensorPosition.Front
             - carla.SensorPosition.FrontLeft
             - carla.SensorPosition.FrontRight
@@ -150,9 +196,10 @@ class my_agent(AutonomousAgent):
             - carla.SensorPosition.Back
 
         Returns:
-        carla.VehicleVelocityControl: The control input to the simulator, even if the control decision hasn't been updated
-        during the time-step. Returning None or any other type will cause an error.
+        carla.VehicleVelocityControl: The control input to the simulator, even
+        if the control decision hasn't been updated during the time-step.
+        Returning None or any other type will cause an error.
         """
 
         # Return the control input to the simulator
-        return carla.VehicleVelocityControl(0.0, 0.0)
+        return VehicleVelocityControl(0.0, 0.0)
